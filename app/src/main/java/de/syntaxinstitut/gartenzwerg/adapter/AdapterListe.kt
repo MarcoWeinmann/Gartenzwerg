a5895a3c21c06650897695af313c2152596f64dd
marcoweinmann <marco.wm94@gmail.com>
2022-09-05 14:53:31 +0200

    gemüseliste fragment hinzugefügt

--- app/src/main/java/de/syntaxinstitut/gartenzwerg/adapter/AdapterListe.kt
+++ app/src/main/java/de/syntaxinstitut/gartenzwerg/adapter/AdapterListe.kt
@@ -1,20 +1,18 @@
 package de.syntaxinstitut.gartenzwerg.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
+import de.syntaxinstitut.gartenzwerg.GemueseListeFragmentDirections
 import de.syntaxinstitut.gartenzwerg.data.models.Pflanzen
-import de.syntaxinstitut.gartenzwerg.databinding.FragmentHomeBinding
 import de.syntaxinstitut.gartenzwerg.databinding.ListItemBinding
 import de.syntaxinstitut.gartenzwerg.ui.home.HomeFragmentDirections
 
-class AdapterHome (
+class AdapterListe (
     private val dataset: List<Pflanzen>
 
-        ) : RecyclerView.Adapter<AdapterHome.ItemViewHolder>(){
+        ) : RecyclerView.Adapter<AdapterListe.ItemViewHolder>(){
 
             class ItemViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -36,7 +34,7 @@ class AdapterHome (
         holder.binding.ivListItem.setImageResource(item.pictureResource)
         holder.binding.tvListItem.text = item.name
         holder.binding.cvListItem.setOnClickListener{
-            holder.itemView.findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToDetailFragment(name = item.name, bild = item.pictureResource, text = item.text, id = position+1))
+            holder.itemView.findNavController().navigate(GemueseListeFragmentDirections.actionGemueseListeFragmentToDetailFragment(name = item.name, bild = item.pictureResource, text = item.text, id = position+1))
         }
     }
 
--- app/src/main/java/de/syntaxinstitut/gartenzwerg/ui/home/HomeViewModel.kt
+++ app/src/main/java/de/syntaxinstitut/gartenzwerg/ui/home/HomeViewModel.kt
@@ -9,24 +9,4 @@ import androidx.lifecycle.MutableLiveData
  */
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
-    /* -------------------- Klassen Variablen -------------------- */
-	
-    /** Signal um zu signalisieren, dass zum zweiten Fragment gewechselt werden soll */
-    var navigateToFragmentTwo = MutableLiveData(false)
-
-    /* -------------------- Öffentliche Funktionen -------------------- */
-
-    /**
-     * Mit dieser Funktion wird der Trigger ausgelöst um zum zweiten Fragment zu wechseln
-     */
-    fun navigateToFragmentTwo() {
-        navigateToFragmentTwo.value = true
-    }
-
-    /**
-     * Setzt alle Werte der Variablen auf ihren "Werkszustand" zurück
-     */
-    fun resetAllValues() {
-        navigateToFragmentTwo.value = false
-    }
 }

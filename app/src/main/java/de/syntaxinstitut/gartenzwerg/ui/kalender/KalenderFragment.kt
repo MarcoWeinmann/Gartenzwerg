57345010a504c781e3721f038304b7ea0b781ddf
marcoweinmann <marco.wm94@gmail.com>
2022-09-06 14:12:03 +0200

    Revert "dropdown menu in kalenderfragment eingebaut"
    This reverts commit 97494c35

--- app/src/main/java/de/syntaxinstitut/gartenzwerg/ui/kalender/KalenderFragment.kt
+++ app/src/main/java/de/syntaxinstitut/gartenzwerg/ui/kalender/KalenderFragment.kt
@@ -5,41 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import de.syntaxinstitut.gartenzwerg.R
-import de.syntaxinstitut.gartenzwerg.databinding.FragmentKalenderBinding
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [KalenderFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class KalenderFragment : Fragment() {
-
-        private lateinit var binding: FragmentKalenderBinding
-
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
-    }
-        //Dropdownmenu
-    override fun onResume() {
-        super.onResume()
-        val gemuese = resources.getStringArray(R.array.Gemuese)
-        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, gemuese)
-        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        binding = FragmentKalenderBinding.inflate(inflater, container, false)
-
-
-
         // Inflate the layout for this fragment
-        return binding.root
+        return inflater.inflate(R.layout.fragment_kalender, container, false)
     }
 
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment KalenderFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            KalenderFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
